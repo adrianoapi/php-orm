55fd266d4f7f84bc1d7b0d2641bdfa138ec1bd4b
Adriano A Costa <sdcomputadores@gmail.com>
2018-01-19 03:35:25 -0200

    criando a persistência do modelo

--- src/Model.php
+++ src/Model.php
@@ -7,34 +7,47 @@ use App\ORM\Drivers\DriverStrategy;
 class Model
 {
 
+    protected $driver;
+
     public function setDriver(DriverStrategy $driver)
     {
-        return $driver;
+        $this->driver;
+        $this;
     }
 
     protected function getDriver()
     {
-        
+        $driver;
     }
 
     public function save()
     {
-        
+        $this->getDriver()
+                ->save($this)
+                ->exec();
     }
 
     public function findAll(array $conditions = [])
     {
-        
+        return $this->getDriver()
+                        ->select($conditions)
+                        ->exec()
+                        ->all();
     }
 
     public function findFirst($id)
     {
-        
+        return $this->getDriver()
+                        ->select(['id' => $id])
+                        ->exec()
+                        ->first();
     }
 
     public function delete()
     {
-        
+        $this->getDriver()
+                ->delete(['id' => $id])
+                ->exec();
     }
 
 }
